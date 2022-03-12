1b4dd8eb16d7791e76d1fdd6d6de639e56f6e023
mxt629 <xuantien.06021990@gmail.com>
2022-03-12 09:59:34 +0700

    Initial Commit

---
+++ Bai tap 7/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Bai tap 7
+//
+//  Created by Little Chick on 12/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
